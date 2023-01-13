05d1412ef5c01fcc3d964b7ff178fa91c2b27366
Ahmadnur415 <ahmadnursahid12345@gmail.com>
2023-01-13 09:00:44 +0700

    rewrite navbar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -4,25 +4,9 @@ $(document).ready(function() {
     $(".navbar-burger").click(function() {
   
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $("#nav_blog").toggleClass("in");
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
   
     });
 });
-
-// https://stackoverflow.com/questions/22541364/sticky-navbar-onscroll
-// $(".header-sticky").toggleClass('is-fixed-top',jQuery(window).scrollTop() > 50);
-// var num = 0;
-// $(window).scroll(function () {
-//     $(".navbar").toggleClass('is-fixed-top',$(window).scrollTop() > 0);
-//     $("body").toggleClass('has-navbar-fixed-top',$(window).scrollTop() > 0);
-// });
-
-
-// // $(window).bind('scroll', function () {
-// //     if ($(window).scrollTop() > num) {
-// //         $('.navbar').addClass('is-fixed-top');
-// //     } else {
-// //         $('.navbar').removeClass('is-fixed-top');
-// //     }
-// // });
