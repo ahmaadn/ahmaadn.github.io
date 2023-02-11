c78567765be319f0cb7c2e975a75fb03f90fa9d7
Ahmadnur415 <ahmadnursahid12345@gmail.com>
2023-02-11 11:20:28 +0700

    create page search

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -14,15 +14,8 @@ $(document).ready(function() {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-
-    $(".nav-custum").toggleClass("in", document.body.scrollTop > 80 || document.documentElement.scrollTop > 80)
-
-
-//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//     document.getElementById("navbar").style.padding = "30px 10px";
-//     document.getElementById("logo").style.fontSize = "25px";
-//   } else {
-//     document.getElementById("navbar").style.padding = "80px 10px";
-//     document.getElementById("logo").style.fontSize = "35px";
-//   }
+    $(".navbar").toggleClass(
+        "in has-shadow",
+        document.body.scrollTop > 80 || document.documentElement.scrollTop > 80
+    )
 }
