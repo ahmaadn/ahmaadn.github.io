fe12c25c991db8dd579e1c24f9467799c9f7641c
Ahmadnur415 <ahmadnursahid12345@gmail.com>
2023-02-19 11:41:17 +0700

    update js untuk tabs dan navbar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,20 +1,18 @@
 $(document).ready(function() {
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function() {
-  
+
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         $("#nav_blog").toggleClass("in");
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
-  
+
     });
 });
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
+window.onscroll = function() {
     $(".navbar").toggleClass(
         "in has-shadow",
         document.body.scrollTop > 80 || document.documentElement.scrollTop > 10
     )
-}
+};
--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,43 +1,37 @@
-const removeActiveClasses = function (ulElement) {
-    const lis = ulElement.querySelectorAll('li');
-    Array.prototype.forEach.call(lis, function(li) {
-        li.classList.remove('is-active');
-    });
+$(document).ready(function () {
+  function removeActiveClasses(ulElement) {
+    var lis = $(ulElement).find('li');
+    lis.removeClass('is-active');
   }
 
-  const getChildPosition = function (element) {
-        var parent = element.parentNode;
-        var i = 0;
-        for (var i = 0; i < parent.children.length; i++) {
-            if (parent.children[i] === element) {
-                return i;
-            }
-        }
-
-        throw new Error('No parent found');
+  function getChildPosition(element) {
+    var parent = element.parentNode;
+    var i = 0;
+    for (var i = 0; i < parent.children.length; i++) {
+      if (parent.children[i] === element) {
+        return i;
+      }
     }
 
-window.addEventListener('load', function () {
-    const tabLinks = document.querySelectorAll('div.tabs ul li a');
+    throw new Error('No parent found');
+  }
 
-    Array.prototype.forEach.call(tabLinks, function(link) {
-      link.addEventListener('click', function (event) {
-        event.preventDefault();
+  $('div.tabs ul li a').click(function (event) {
+    event.preventDefault();
 
-        liTab = link.parentNode;
-        ulTab = liTab.parentNode;
-        position = getChildPosition(liTab);
-        if (liTab.className.includes('is-active')) {
-          return;
-        }
+    var liTab = $(this).parent();
+    var ulTab = liTab.parent();
+    var position = getChildPosition(liTab[0]);
+    if (liTab.hasClass('is-active')) {
+      return;
+    }
 
-        removeActiveClasses(ulTab);
-        tabContentId = ulTab.getAttribute('data-tab');
-        tabContentElement = document.getElementById(tabContentId);
-        removeActiveClasses(tabContentElement);
+    removeActiveClasses(ulTab);
+    var tabContentId = ulTab.attr('data-tab');
+    var tabContentElement = $('#' + tabContentId);
+    removeActiveClasses(tabContentElement);
 
-        tabContentElement.querySelectorAll('li')[position].classList.add('is-active');
-        liTab.classList.add('is-active');
-      }, false);
-    });
+    tabContentElement.find('li')[position].classList.add('is-active');
+    liTab.addClass('is-active');
+  });
 });
